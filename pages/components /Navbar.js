fb2126393aa935f115c36354d330d4addac37114
7sMedia <jay7nyc@gmail.com>
2025-06-22 19:26:26 -0400

    Navbar.js

---
+++ pages/components /Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-white border-b shadow px-4 py-2 flex justify-between items-center">
+      <span className="font-bold text-xl">Stivio</span>
+    </nav>
+  )
+}
