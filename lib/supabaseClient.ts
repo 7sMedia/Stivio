5b3ac1892690c546bf5b7d89166cfe04bca71984
7sMedia <jay7nyc@gmail.com>
2025-06-24 16:37:38 -0400

    supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = "https://phwklfbggmalosniqhjb.supabase.co"; // Replace with your real URL
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBod2tsZmJnZ21hbG9zbmlxaGpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3NDgyMTQsImV4cCI6MjA2NjMyNDIxNH0._U6PWjqHA2b7SfB-NqWFs0mYCgfS_N0TSsUWnhXFiT8"; // Replace with your real key
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
