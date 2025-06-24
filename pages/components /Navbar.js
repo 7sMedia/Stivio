22363a009173e51cc9ae67d3f4b00cdb9c804453
7sMedia <jay7nyc@gmail.com>
2025-06-24 15:39:24 -0400

    Delete pages directory

--- pages/components /Navbar.js
+++
@@ -1,7 +0,0 @@
-export default function Navbar() {
-  return (
-    <nav className="bg-white border-b shadow px-4 py-2 flex justify-between items-center">
-      <span className="font-bold text-xl">Stivio</span>
-    </nav>
-  )
-}
