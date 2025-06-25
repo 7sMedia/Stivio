64028946b38c731bc1b554410e962b22d8037c23
7sMedia <jay7nyc@gmail.com>
2025-06-25 02:48:51 -0400

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
-// /app/layout.tsx
-import '../styles/globals.css';
+import { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="min-h-screen bg-gradient-to-br from-indigo-900 via-violet-900 to-black antialiased">
