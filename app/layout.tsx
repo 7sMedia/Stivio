36cf9b844428153c92e552e354112a5d86aa8b77
7sMedia <jay7nyc@gmail.com>
2025-06-25 03:17:26 -0400

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body className="min-h-screen bg-gradient-to-br from-indigo-900 via-violet-900 to-black antialiased">
+    <html lang="en" className="min-h-screen h-full">
+      <body className="min-h-screen h-full bg-gradient-to-br from-indigo-900 via-violet-900 to-black antialiased">
         {children}
       </body>
     </html>
