6e66da21b85dcfae833dcc0968cb62f08b991925
7sMedia <jay7nyc@gmail.com>
2025-07-05 19:51:40 -0400

    ResponsiveContainer.tsx

--- components/ui/ResponsiveContainer.tsx
+++ components/ui/ResponsiveContainer.tsx
@@ -1,9 +1,8 @@
-// app/components/ui/ResponsiveContainer.tsx
 import { ReactNode } from "react";
 
 export default function ResponsiveContainer({ children }: { children: ReactNode }) {
   return (
-    <div className="w-full max-w-md sm:max-w-lg md:max-w-2xl lg:max-w-4xl xl:max-w-6xl mx-auto px-4">
+    <div className="w-full max-w-md sm:max-w-xl md:max-w-3xl lg:max-w-5xl xl:max-w-7xl 2xl:max-w-none mx-auto px-4">
       {children}
     </div>
   );
