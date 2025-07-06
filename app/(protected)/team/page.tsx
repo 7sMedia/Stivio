8f7a0d72dc8349d2656f834dd08414a0c87d7f25
7sMedia <jay7nyc@gmail.com>
2025-07-05 23:12:12 -0400

    page.tsx

--- app/(protected)/team/page.tsx
+++ app/(protected)/team/page.tsx
@@ -5,9 +5,12 @@ import React from "react";
 
 export default function TeamPage() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-white mb-4">Team</h1>
-      {/* TODO: your team management UI goes here */}
+    <div className="min-h-screen flex flex-col bg-[#141518] text-white">
+      {/* NavBar, Sidebar, TopBar will be rendered by the layout */}
+      <main className="flex-1 p-8">
+        <h1 className="text-3xl font-bold mb-4">Team</h1>
+        {/* TODO: your team management UI goes here */}
+      </main>
     </div>
   );
 }
