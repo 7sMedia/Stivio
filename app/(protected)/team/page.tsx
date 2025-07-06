1f3494310aec87851d2feae1410ec74a41770e83
7sMedia <jay7nyc@gmail.com>
2025-07-05 23:04:55 -0400

    page.tsx

--- app/(protected)/team/page.tsx
+++ app/(protected)/team/page.tsx
@@ -1,1 +1,18 @@
+// app/(protected)/team/page.tsx
+"use client";
+
+import React from "react";
+// …same imports…
+
+export default function TeamPage() {
+  return (
+    <div className="min-h-screen flex flex-col bg-[#141518] text-white">
+      {/* NavBar, Sidebar, TopBar as above */}
+      <main className="flex-1 p-8">
+        <h1 className="text-3xl font-bold mb-4">Team</h1>
+        {/* team management UI */}
+      </main>
+    </div>
+  );
+}
 
