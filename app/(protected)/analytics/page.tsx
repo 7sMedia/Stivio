2768d3de767907c7f75d9013896c2ce373a4ccf0
7sMedia <jay7nyc@gmail.com>
2025-07-05 23:24:54 -0400

    page.tsx

--- app/(protected)/analytics/page.tsx
+++ app/(protected)/analytics/page.tsx
@@ -1,15 +1,13 @@
 // app/(protected)/analytics/page.tsx
 "use client";
-
 import React from "react";
 
 export default function AnalyticsPage() {
   return (
     <div className="min-h-screen flex flex-col bg-[#141518] text-white">
-      {/* NavBar, Sidebar, TopBar will be rendered by the layout */}
       <main className="flex-1 p-8">
         <h1 className="text-3xl font-bold mb-4">Analytics</h1>
-        {/* TODO: your analytics dashboard/components go here */}
+        {/* your analytics UI */}
       </main>
     </div>
   );
