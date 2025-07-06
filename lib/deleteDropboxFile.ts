f829c1f300c542e364eda68ce2173551c1decfb4
7sMedia <jay7nyc@gmail.com>
2025-07-06 19:11:01 -0400

    Update deleteDropboxFile.ts

--- lib/deleteDropboxFile.ts
+++ lib/deleteDropboxFile.ts
@@ -1,11 +1,11 @@
 import { Dropbox } from "dropbox";
 
-export async function deleteDropboxFile(accessToken: string, filePath: string) {
+export async function deleteDropboxFile(accessToken: string, path: string) {
   const dbx = new Dropbox({ accessToken });
 
   try {
-    const response = await dbx.filesDeleteV2({ path: filePath });
-    return response.result.metadata;
+    await dbx.filesDeleteV2({ path });
+    return true;
   } catch (error) {
     console.error("Error deleting Dropbox file:", error);
     throw error;
