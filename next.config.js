5bca7722e01792f2cfa040a96b59966a11f93076
7sMedia <jay7nyc@gmail.com>
2025-07-06 18:08:32 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  webpack: (config) => {
+    config.ignoreWarnings = [
+      {
+        module: /node_modules\/@supabase\/realtime-js/,
+        message: /the request of a dependency is an expression/,
+      },
+    ];
+    return config;
+  },
 };
 
 module.exports = nextConfig;
