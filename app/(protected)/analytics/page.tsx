e536cfdb1eac5f35a0f55cee2bae5ac57b93d3fd
7sMedia <jay7nyc@gmail.com>
2025-07-05 23:06:21 -0400

    page.tsx

--- app/(protected)/analytics/page.tsx
+++ app/(protected)/analytics/page.tsx
@@ -2,20 +2,22 @@
 "use client";
 
 import React from "react";
-import NavBar from "@components/NavBar";
 import Sidebar from "@components/Sidebar";
 import TopBar from "@components/TopBar";
+import NavBar from "@components/NavBar";
 
 export default function AnalyticsPage() {
-  // If you need auth-guard or data-fetching, do it here…
+  // TODO: replace with real user & nav logic
+  const user = { email: "jay7nyc@hotmail.com" };
+
   return (
-    <div className="min-h-screen flex flex-col bg-[#141518] text-white">
-      <NavBar /*…props…*/ />
-      <div className="flex flex-1">
-        <Sidebar /*…props…*/ />
+    <div className="min-h-screen flex bg-[#141518] text-white">
+      <Sidebar user={user} open={true} onClose={() => {}} />
+      <div className="flex-1 flex flex-col">
+        <TopBar user={user} onLogout={() => window.location.href = "/logout"} onMenuToggle={() => {}} />
         <main className="flex-1 p-8">
           <h1 className="text-3xl font-bold mb-4">Analytics</h1>
-          {/* your analytics dashboard here */}
+          {/* your analytics dashboard goes here */}
         </main>
       </div>
     </div>
