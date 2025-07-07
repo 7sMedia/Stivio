339afd88d9fdb0e8c7015c19e7a42e6f82347ad7
7sMedia <jay7nyc@gmail.com>
2025-07-07 03:03:33 -0400

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,17 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>Beta7</title>
+      <meta name="description" content="AI video creation from still images" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+      {/* Dropbox Chooser SDK */}
+      <script
+        id="dropbox-chooser"
+        src="https://www.dropbox.com/static/api/2/dropins.js"
+        data-app-key={process.env.NEXT_PUBLIC_DROPBOX_APP_KEY}
+      />
+    </>
+  );
+}
