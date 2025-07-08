7710e9cc64ace13650282f4c9616c901fbe0151e
7sMedia <jay7nyc@gmail.com>
2025-07-07 22:16:04 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,28 @@
-// app/layout.tsx
+// /app/layout.tsx
 
 import "../styles/globals.css";
 import { ReactNode } from "react";
-import { ToastProvider } from "@/components/ui/use-toast";
+import Script from "next/script";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className="antialiased overflow-x-hidden dark">
-      <body className="min-h-screen overflow-x-hidden">
-        <ToastProvider>{children}</ToastProvider>
+    <html lang="en" className="min-h-screen h-full dark">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <title>Beta7</title>
+        <meta name="description" content="Beta7 AI Video Generator from Images" />
+        <link rel="icon" href="/favicon.ico" />
+        <Script
+          src="https://www.dropbox.com/static/api/2/dropins.js"
+          id="dropboxjs"
+          data-app-key={process.env.NEXT_PUBLIC_DROPBOX_APP_KEY}
+          strategy="beforeInteractive"
+        />
+      </head>
+      <body className="min-h-screen h-full antialiased bg-black text-white">
+        {children}
       </body>
     </html>
   );
