9e120584841f32f1578b1bf25d2308fb07a9034d
7sMedia <jay7nyc@gmail.com>
2025-07-13 00:33:36 -0400

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-variants";
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+/**
+ * Merge Tailwind and conditional class names.
+ * Works with shadcn/ui + tailwind-merge + clsx.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
